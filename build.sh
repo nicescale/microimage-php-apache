2c7ff2019b8322c936cba1993def9b5063b916ee
shellrock <hanwoody@gmail.com>
2016-02-03 14:27:10 +0800

    add test for apache/php

--- build.sh
+++ build.sh
@@ -3,21 +3,21 @@
 set -e
 
 export CON_NAME=php-apache_t
-export REG_URL=d.nicescale.com:5000
-export IMAGE=php-apache
+export REG_URL=index.csphere.cn
+export IMAGE=microimages/php-apache
 export TAGS="5.6 5.6.10"
 export BASE_IMAGE=microimages/alpine
 
 docker pull $BASE_IMAGE
 
-docker build -t microimages/$IMAGE .
+docker build -t $IMAGE .
+./test.sh
 
-#./test.sh
-
-echo "---> Starting push microimages/$IMAGE:$VERSION"
-
+docker tag -f $IMAGE $REG_URL/$IMAGE
 for t in $TAGS; do
-  docker tag -f microimages/$IMAGE microimages/$IMAGE:$t
+  docker tag -f $IMAGE $REG_URL/$IMAGE:$t
+  docker tag -f $IMAGE $IMAGE:$t
 done
 
-docker push microimages/$IMAGE
+docker push $IMAGE
+docker push $REG_URL/$IMAGE
