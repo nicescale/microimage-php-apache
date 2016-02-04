4e3b0131cf133445e058c45ccc2ba0ff24e24e87
shellrock <hanwoody@gmail.com>
2016-02-04 18:00:11 +0800

    upgrade to php 5.6.17

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -e
 export CON_NAME=php-apache_t
 export REG_URL=index.csphere.cn
 export IMAGE=microimages/php-apache
-export TAGS="5.6 5.6.10"
+export TAGS="5.6 5.6.17"
 export BASE_IMAGE=microimages/alpine
 
 docker pull $BASE_IMAGE
